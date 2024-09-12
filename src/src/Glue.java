48d220bf667c8e4bdcabcd82e6ebd8504751dfce
santi-crown <samtigomusik@gmail.com>
2024-09-11 20:21:17 -0500

    Add folder docs and change project structure

---
+++ src/src/Glue.java
@@ -0,0 +1,19 @@
+public class Glue {
+    private int row;
+    private int col;
+
+    // Constructor
+    public Glue(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    // Métodos getters
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
