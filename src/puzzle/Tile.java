b55e6398c5c9b1f6be0044400125cda3baf6518b
santi-crown <samtigomusik@gmail.com>
2024-09-14 19:44:25 -0500

    Fix relocateTile, now tiles change fisically positions

--- src/puzzle/Tile.java
+++ src/puzzle/Tile.java
@@ -6,22 +6,32 @@ public class Tile extends Rectangle {
     private int size;
     private int padding; // Padding interno
     Color lightBrown = new Color(207, 126, 60);
+    private int xPosition;
+    private int yPosition;
 
     public Tile(int size, char label, int xPosition, int yPosition, int padding) {
         this.size = size;
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
         this.padding = padding; // Inicializa el padding
         this.label = label;
         int effectiveSize = size - 2 * padding; // Tamaño efectivo después de aplicar padding
         // Cambia el tamaño del rectángulo para reflejar el padding
         this.changeSize(size, size);            
         // Cambia el color y mueve el rectángulo visible
-        this.setTileColor(label);
+        this.setTileColor(this.label);
         this.moveHorizontal(xPosition);
         this.moveVertical(yPosition);
+        if (!this.getTileColor().equals(lightBrown)){
+            this.makeVisible();
+        } else {
+            this.makeInvisible();
+        }
         this.makeVisible();
     }
     
     public void setTileColor(char label){
+        
         switch (label) {
             case 'r':
                 color = Color.RED;
@@ -35,15 +45,14 @@ public class Tile extends Rectangle {
             case 'g':
                 color = Color.GREEN;
                 break;
-            case 'n':
+            case '*':
                 color = lightBrown;
                 break;                
             default:
                 color = lightBrown;
                 
-        }
-        
-        this.changeColor(this.color);
+        }        
+        this.changeColor(color);
     }
     
     public Color getTileColor(){
@@ -52,6 +61,14 @@ public class Tile extends Rectangle {
     
     public char getLabel(){
         return label;
+    }
+    
+    public int getXposition(){
+        return xPosition;
+    }
+    
+    public int getYposition(){
+        return yPosition;
     }
     
 }
