2c84e54e2d499fc9bf3f4481ecc1331cb1bcae73
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2024-12-03 21:43:04 -0500

    Add second constructor poobvszombies - player vs machine

--- src/main/java/domain/Zombies.java
+++ src/main/java/domain/Zombies.java
@@ -5,9 +5,22 @@ import java.util.ArrayList;
 public class Zombies extends Team {
     public static final String NAME = "Zombies";
     protected static final int BRAIN_AMOUNT_INCREASE = 50; // Adjusted resource increase for Zombies
+    private static final ArrayList<String> ORIGINAL_ZOMBIES = new ArrayList<>();
+
+    static {
+        ORIGINAL_ZOMBIES.add("Basic");
+        ORIGINAL_ZOMBIES.add("Brainstein");
+        ORIGINAL_ZOMBIES.add("BucketHead");
+        ORIGINAL_ZOMBIES.add("Conehead");
+        ORIGINAL_ZOMBIES.add("ECIZombie");
+    }
 
     public Zombies(int resourceCounter, ArrayList<String> characters){
         super(resourceCounter, characters);
+    }
+
+    public Zombies(){
+        super(0, ORIGINAL_ZOMBIES);
     }
 
     @Override
--- src/main/java/domain/ZombiesOriginal.java
+++ src/main/java/domain/ZombiesOriginal.java
@@ -1,4 +1,43 @@
 package domain;
 
-public class ZombiesOriginal {
+public class ZombiesOriginal extends HumanPlayer{
+    public static final int ORIGINAL_SPAWN_TIME = 10;
+    private int hordersNumber;
+    private float horderTime;
+
+    public ZombiesOriginal(int hordersNumber, float matchTime) {
+        super("Original Zombies machine");
+        Team zombiesHordersTeam = new Zombies();// El constructor de Zombies no recibe parámetros porque es zombie original
+        this.team = zombiesHordersTeam;
+        this.hordersNumber = hordersNumber;
+        this.horderTime = matchTime / hordersNumber;
+    }
+
+    public void setOriginalStrategy() {
+        // Implementation for setting the original strategy
+    }
+
+    public void spawnZombie(Zombie zombie, int line, int xPos) {
+        // Implementation for spawning a zombie
+    }
+
+    public void setHordersNumbers(int number) {
+        this.hordersNumber = number;
+    }
+
+    public void setHordersTime(float matchTime) {
+        this.horderTime = matchTime / hordersNumber;
+    }
+
+    public int getHordersNumber() {
+        return hordersNumber;
+    }
+
+    public float getHorderTime(float matchTime) {
+        return horderTime;
+    }
+
+    public void playTurn() {
+        // Implementation for playing a turn
+    }
 }
